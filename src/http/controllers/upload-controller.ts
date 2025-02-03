d98efe3fa466cfa19984a274a0394e4d1bfc7759
Seila-Dev <erickrodrigues3975@gmail.com>
2025-02-03 14:57:47 -0300

    chore: refatorado multer e criado rota com sucesso

--- src/http/controllers/upload-controller.ts
+++ src/http/controllers/upload-controller.ts
@@ -1,18 +1,39 @@
 import { Request, Response } from "express"
+import path from 'path'
+import { prisma } from '../../prisma/prisma'
 
 
 
 export class UploadController {
-    async upload(request: Request, response: Response) {
-        if (request.file) {
-            response.json({
-                message: "File uploaded",
-                file: request.file
-            })
-        } else {
-            response.status(400).json({
-                message: "No file uploaded"
-            })
+    async upload(request: Request, response: Response): Promise<void> {
+        if (!request.file) {
+            response.status(400).json({ message: 'No file uploaded' });
+            return
         }
+        try {
+            const filePath = path.join(request.file.filename)
+        const { name, price, description, highlight, category_id, size_id, color_id } = request.body
+
+        const product = await prisma.product.create({
+            data: {
+                image: filePath,
+                name,
+                description,
+                category_id: parseInt(category_id),
+                size_id: parseInt(size_id),
+                color_id: parseInt(color_id),
+                price,
+                highlight: Boolean(highlight),
+                created_at: new Date(),
+                updated_at: new Date(),
+            } 
+        })
+            response.status(201).json(product)
+        } catch (error) {
+            console.log(error)
+            response.status(400).send({ message: "Houve um erro no try"})
+        }
+        
+        
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import { app } from "./app"
 import productsRoutes from "./routes/products-routes"
+import express from 'express'
 import uploadRoutes from "./routes/upload-routes"
+import path from 'path'
 
 const port = 3000
+
+const publicPath = path.join(process.cwd(), 'uploads'); // usando process.cwd() ao invés de __dirname
+app.use('/uploads', express.static(publicPath));
+
 
 app.listen(port, () => {
     console.log('HTTP Server Running!')
