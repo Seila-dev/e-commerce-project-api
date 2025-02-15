93d4668592b9aa4fa7b0c650d1559c2aaaee1871
Seila-Dev <erickrodrigues3975@gmail.com>
2025-02-15 20:38:02 -0300

    fix: removed imgur api due to errors

--- src/http/controllers/upload-controller.ts
+++ src/http/controllers/upload-controller.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from "express"
 import path from 'path'
 import { prisma } from '../../prisma/prisma'
-import axios from 'axios'
 
-
-const IMGUR_CLIENT_ID = '673aa4e6cc5ef90'
 
 export class UploadController {
     async upload(request: Request, response: Response): Promise<void> {
@@ -13,28 +10,12 @@ export class UploadController {
             return
         }
         try {
-            // const filePath = path.join(request.file.filename)
-            const file = request.file
+            const filePath = path.join(request.file.filename)
             const { name, ean, price, description, highlight, categoryId, sizeId, colorId } = request.body
-
-            const imgurResponse = await axios.post(
-                'https://api.imgur.com/3/image',
-                {
-                    image: file.buffer.toString('base64'),
-                    type: 'base64'
-                },
-                {
-                    headers: {
-                        Authorization: `Client-ID ${IMGUR_CLIENT_ID}`,
-                    },
-                }
-            )
-
-            const imageUrl = imgurResponse.data.data.link;
 
             const product = await prisma.product.create({
                 data: {
-                    image: imageUrl,
+                    image: filePath,
                     name,
                     description,
                     categoryId: parseInt(categoryId),
